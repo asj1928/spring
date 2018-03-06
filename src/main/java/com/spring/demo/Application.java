f0c73b956460ff31994b011f50d26ac727e0284c
ajay <ajay.singh@tothenew.com>
2018-03-06 21:52:30 +0530

    Q6 byname

--- src/main/java/com/spring/demo/Application.java
+++ src/main/java/com/spring/demo/Application.java
@@ -6,17 +6,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Application {
     public static void main ( String[] args ) {
         ApplicationContext context=new ClassPathXmlApplicationContext (" config.xml" ) ;
-//        Database database=context.getBean ( Database.class );
-//                                                                          Q1
-//        System.out.println (database );
+////        Database database=context.getBean ( Database.class );
+////                                                                          Q1
+////        System.out.println (database );
+//
+//        Restaurant  teaRestaurant=context.getBean ( "TeaRestaurant",Restaurant.class );
+//        teaRestaurant.getHotDrink ().prepareHotDrink ();                    //Q3
+//
+////        Restaurant expressTeaRestaurant=context.getBean ( "expressTeaRestaurant",Restaurant.class );
+////        expressTeaRestaurant.getHotDrink ().prepareHotDrink ();             //Q4
+//
+//        Complex complex=context.getBean ( Complex.class );
+//        System.out.println (complex );                                     //Q5
 
-        Restaurant  teaRestaurant=context.getBean ( "TeaRestaurant",Restaurant.class );
-        teaRestaurant.getHotDrink ().prepareHotDrink ();                    //Q3
+        Restaurant rs=context.getBean ( "RestaurantQ6a",Restaurant.class );
+        rs.getHotDrink ().prepareHotDrink ();                             //Q6 byname
 
-        Restaurant expressTeaRestaurant=context.getBean ( "expressTeaRestaurant",Restaurant.class );
-        expressTeaRestaurant.getHotDrink ().prepareHotDrink ();             //Q4
-
-        Complex complex=context.getBean ( Complex.class );
-        System.out.println (complex );                                     //Q5
     }
 }
