8df41ba98e4331cefc6e676bdbe5e45b407fd75d
ajay <ajay.singh@tothenew.com>
2018-03-06 15:20:29 +0530

    com1

--- src/main/java/com/spring/demo/Application.java
+++ src/main/java/com/spring/demo/Application.java
@@ -1,7 +1,13 @@
 package com.spring.demo;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main ( String[] args ) {
+        ApplicationContext context=new ClassPathXmlApplicationContext ("config.xml" ) ;
+        Database database=context.getBean ( Database.class );
+
+        System.out.println (database );
     }
 }
